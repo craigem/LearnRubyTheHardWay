e78601a1e7b78895128efb382432fc655235b2d5
Craige McWhirter <craige@mcwhirter.com.au>
2014-08-14 21:43:00 +1000

    Completed ex11, study drill 3

---
+++ ex11_3.rb
@@ -0,0 +1,8 @@
+print "Where are you now? "
+now_where = gets.chomp
+print "Where do you live? "
+live_where = gets.chomp
+print "Where would you rather be? "
+be_where = gets.chomp
+
+puts "\nSo you're curently in #{now_where},\n\tyet you live in #{live_where}\n\t\tbut you'd rather be #{be_where}.\nFair enough."
