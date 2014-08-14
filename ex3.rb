2bbbab5c71c02273343b64b1c980e02affd00111
Craige McWhirter <craige@mcwhirter.com.au>
2014-08-14 12:23:30 +1000

    Completed ex3, Study drill 1

---
+++ ex3.rb
@@ -0,0 +1,36 @@
+# Prints a statement to STDOUT
+puts "I will now count my chickens:"
+
+# Calculates the number of hens and prints it to STDOUT
+puts "Hens #{25 + 30 / 6}"
+# Calculates the number of roosters and prints it to STDOUT
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# Prints a statement to STDOUT
+puts "Now I will count the eggs:"
+
+# Calculates the number of eggs and prints it to STDOUT
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Prints a statement to STDOUT
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# Evaluates two equations and prints the result to STDOUT
+puts 3 + 2 < 5 - 7
+# Calculates an equation and prints the result to STDOUT
+puts "What is 3 + 2? #{3 + 2}"
+# Calculates an equation and prints the result to STDOUT
+puts "What is 5 - 7? #{5 - 7}"
+
+# Prints a statement to STDOUT
+puts "Oh, that's why it's false."
+
+# Prints a statement to STDOUT
+puts "How about some more."
+
+# Calculates an equation and prints the result to STDOUT
+puts "It is greater? #{5 > -2}"
+# Calculates an equation and prints the result to STDOUT
+puts "Is it greater or equal? #{5 >= -2}"
+# Calculates an equation and prints the result to STDOUT
+puts "Is it less or equal? #{5 <= -2}"
