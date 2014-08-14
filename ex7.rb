59709c6fd7b4afed250e73a2ee9493f6e87ac5ca
Craige McWhirter <craige@mcwhirter.com.au>
2014-08-14 19:51:38 +1000

    Completed ex7, Study drill 1wq

--- ex7.rb
+++ ex7.rb
@@ -1,8 +1,13 @@
+# Prints the string to STDOUT.
 puts "Mary had a litle lamb."
+# Prints the string to STDOUT.
 puts "It's fleece is white as #{'snow'}."
+# Prints the string to STDOUT.
 puts "And everywhere that Mary went."
+# Prints the string to STDOUT 10 times.
 puts "." * 10  # what'd that do?
 
+# Sets the value of end1 throught to end12
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -17,5 +22,7 @@ end11 = "e"
 end12 = "r"
 
 # watch that print vs. puts on this line what's it do?
+# Concatenates the variables and prints them to STDOUT without a newline
 print end1 + end2 + end3 + end4 + end5 + end6
+# Concatenates the variables and prints them to STDOUT with a newline
 puts end7 + end8 + end9 + end10 + end11 + end12
