36a5fd28f7715e355b1ef1a78e5553ff2cffe7ad
Craige McWhirter <craige@mcwhirter.com.au>
2014-08-14 19:36:17 +1000

    Completed ex6, study drill 1

--- ex6.rb
+++ ex6.rb
@@ -1,21 +1,36 @@
+# Set the value for the types of people
 types_of_people = 10
+# Set the value of x to include the type of people
 x = "There are #{types_of_people} types of people."
+# Set the value of binary
 binary = "binary"
+# Set the value of do-not
 do_not = "don't"
+# Set the value of y to include binary an dd_not
 y = "Those who know #{binary} and those who #{do_not}."
 
+# Print x to STDOUT
 puts x
+# Print y to STDOUT
 puts y
 
+# Print statement and variable x to STDOUT
 puts "I said: #{x}."
+# Print statement and variable y to STDOUT
 puts "I also said: '#{y}'."
 
+# Set the value of hilarious
 hilarious = false
+# Set the value of joke_evaluation and include the hilarious value.
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
+# Print joke_evalution to STDOUT
 puts joke_evaluation
 
+# Set the value of w
 w = "This is the left side of..."
+# Set the value of e
 e = "a string with a right side."
 
+# Print the values of w and e to STDOUT
 puts w + e
