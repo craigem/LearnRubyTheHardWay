fd50ba83e9dd0ee453399da2d87b2c0e9f0f9a1d
Craige McWhirter <craige@mcwhirter.com.au>
2014-08-14 18:23:37 +1000

    Completed ex4, Study drill 3

--- ex4.rb
+++ ex4.rb
@@ -1,10 +1,18 @@
+# Set the number of cars
 cars = 100
+# Set the number of available spaces
 space_in_a_car = 4.0
+# Set the number of drivers
 drivers = 30
+# Set the number of passengers
 passengers = 90
+# Calculate and set the number of cars not driven
 cars_not_driven = cars - drivers
+# Set the number of cars driven
 cars_driven = drivers
+# Calculate and set the carpool capacity
 carpool_capacity = cars_driven * space_in_a_car
+# Calculate and set the average passengers per car
 average_passengers_per_car = passengers / cars_driven
 
 
