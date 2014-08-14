a80d0e06fe98a9be19a58f8597990b6f863bcda3
Craige McWhirter <craige@mcwhirter.com.au>
2014-08-14 12:09:16 +1000

    Completed ex2, Study drill 2

--- ex2.rb
+++ ex2.rb
@@ -3,7 +3,7 @@
 
 puts "I could have code like this." # and the comment after is ignored
 
-# You can also use a comment to "diable" or comment out a piece of code:
+# You can also use a comment to "disable" or comment out a piece of code:
 # puts "This won't run."
 
 puts "This will run."
