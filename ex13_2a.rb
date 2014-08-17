843f7a192d9a7c07b31371e1326939e13839f3ac
Craige McWhirter <craige@mcwhirter.com.au>
2014-08-18 07:20:46 +1000

    Completed ex13, study drill 2

---
+++ ex13_2a.rb
@@ -0,0 +1,4 @@
+arg1, arg2 = ARGV
+
+puts "Your first variable is: #{arg1}"
+puts "Your second variable is: #{arg2}"
---
+++ ex13_2b.rb
@@ -0,0 +1,6 @@
+arg1, arg2 , arg3, arg4 = ARGV
+
+puts "Your first variable is: #{arg1}"
+puts "Your second variable is: #{arg2}"
+puts "Your third variable is: #{arg3}"
+puts "Your fourth variable is: #{arg4}"
