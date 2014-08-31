99475b9225c54380a1c895f8a6aa1f4a56338cec
Craige McWhirter <craige@mcwhirter.com.au>
2014-09-01 07:05:09 +1000

    Completed ex19, study drill 3

--- ex19.rb
+++ ex19.rb
@@ -44,10 +44,28 @@ cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
 
 def my_own_design(pants, style, reggies )
   puts "\nIt appears you #{pants} wearing pants. Good on you!"
-  puts "They looks like #{style}. Nice work. You carry it off."
+  puts "They look like #{style}. Nice work. You carry it off."
   puts "Oh, you #{reggies} wearing reggies. That's possibly too much information."
   puts "You're going to stop traffic."
 end
 
 puts "\n1. Calling it directly:"
 my_own_design("are", "footy shorts", "are not")
+
+puts "\n2. Adding Stings:"
+my_own_design("are" + " " + "not", "naked", "are" + " " + "not")
+
+puts "\n3. Using Variables:"
+pants = "are"
+style = "fisherman's pants"
+reggies = "are not"
+my_own_design(pants, style, reggies)
+
+puts "\n4. Calling for input:"
+print "Are you wearin pants?: "
+pants = $stdin.gets.chomp
+print "What style of pants?: "
+style = $stdin.gets.chomp
+print "Are you wearing reggies?: "
+reggies = $stdin.gets.chomp
+my_own_design(pants, style, reggies)
