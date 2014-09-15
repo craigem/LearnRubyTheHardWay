768645d426d48292328fb3a6910151d07f5b5906
Craige McWhirter <craige@mcwhirter.com.au>
2014-09-15 10:28:55 +1000

    MAde further progress on ex25

--- ex25.rb
+++ ex25.rb
@@ -24,7 +24,23 @@ module Ex25
   end
 
   # Takes in a full sentence and returns the sorted words.
-  def Ex245.sort_sentence(sentence)
+  def Ex25.sort_sentence(sentence)
     words = Ex25.break_words(sentence)
     return Ex25.sort_words(words)
   end
+
+  # Prints the first and last owrds of the sentence
+  def Ex25.print_first_and_last(sentence)
+    words = Ex25.break_words(sentence)
+    Ex25.print_first_word(words)
+    Ex25.print_last_word(words)
+  end
+
+  # Sorts the words then prints the first and last one
+  def Ex25.print_first_and_last_sorted(sentence)
+    words = Ex25.sort_sentences(sentence)
+    Ex25.print_first_word(words)
+    Ex25.print_last_word(word)
+  end
+
+end
