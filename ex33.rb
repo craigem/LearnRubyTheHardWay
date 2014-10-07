b2504c8b6c933a74dc4aaf8af6255d0f045fdcc7
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-07 16:08:56 +1100

    Made a start on ex33, study drill 1

--- ex33.rb
+++ ex33.rb
@@ -1,14 +1,18 @@
-i = 0
-numbers = []
+def numberCount(a)
+  i = 0
+  numbers = []
 
-while i < 6
-  puts "At the top i is #{i}"
-  numbers.push(i)
+  while i < a
+    puts "At the top i is #{i}"
+    numbers.push(i)
 
-  i += 1
-  puts "Numbers now: ", numbers
-  puts "At the bottom i is #{i}"
+    i += 1
+    puts "Numbers now: ", numbers
+    puts "At the bottom i is #{i}"
+  end
 end
+
+numberCount(8)
 
 puts "The numbers: "
 
