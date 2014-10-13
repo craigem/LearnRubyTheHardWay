c71133dd91c56156cad3049230b4d5b6268e02c9
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-13 23:07:50 +1100

    Corrected issues identified by rubocop

--- ex01.rb
+++ ex01.rb
@@ -1,9 +1,9 @@
-#puts "Hello World!"
-#puts "Hello Again"
-#puts "I like typing this."
-#puts "This is fun."
-#puts "Yay! Printing."
-#puts "I'd much ratherr you 'not'."
-#puts 'I "said" do not touch this.'
-#puts "This is another printed line"
-puts "Print only this line"
+# puts "Hello World!"
+# puts "Hello Again"
+# puts "I like typing this."
+# puts "This is fun."
+# puts "Yay! Printing."
+# puts "I'd much ratherr you 'not'."
+# puts 'I "said" do not touch this.'
+# puts "This is another printed line"
+puts 'Print only this line'
