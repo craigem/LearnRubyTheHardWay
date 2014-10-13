9a5dae146cdbdacf1d9834f9d12b5c997f91d649
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-13 23:38:00 +1100

    Corrected issues identified by rubocop

--- ex07.rb
+++ ex07.rb
@@ -1,25 +1,25 @@
 # Prints the string to STDOUT.
-puts "Mary had a litle lamb."
+puts 'Mary had a litle lamb.'
 # Prints the string to STDOUT.
 puts "It's fleece is white as #{'snow'}."
 # Prints the string to STDOUT.
-puts "And everywhere that Mary went."
+puts 'And everywhere that Mary went.'
 # Prints the string to STDOUT 10 times.
-puts "." * 10  # what'd that do?
+puts '.' * 10  # what'd that do?
 
 # Sets the value of end1 throught to end12
-end1 = "C"
-end2 = "h"
-end3 = "e"
-end4 = "e"
-end5 = "s"
-end6 = "e"
-end7 = "B"
-end8 = "u"
-end9 = "r"
-end10 = "g"
-end11 = "e"
-end12 = "r"
+end1 = 'C'
+end2 = 'h'
+end3 = 'e'
+end4 = 'e'
+end5 = 's'
+end6 = 'e'
+end7 = 'B'
+end8 = 'u'
+end9 = 'r'
+end10 = 'g'
+end11 = 'e'
+end12 = 'r'
 
 # watch that print vs. puts on this line what's it do?
 # Concatenates the variables and prints them to STDOUT without a newline
