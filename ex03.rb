81cf624ac18799b11af76bde877dae9d13a0ee2f
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-13 23:14:31 +1100

    Corrected issues identified by rubocop

--- ex03.rb
+++ ex03.rb
@@ -1,5 +1,5 @@
 # Prints a statement to STDOUT
-puts "I will now count my chickens:"
+puts 'I will now count my chickens:'
 
 # Calculates the number of hens and prints it to STDOUT
 puts "Hens #{25.0 + 30.0 / 6.0}"
@@ -7,13 +7,13 @@ puts "Hens #{25.0 + 30.0 / 6.0}"
 puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
 
 # Prints a statement to STDOUT
-puts "Now I will count the eggs:"
+puts 'Now I will count the eggs:'
 
 # Calculates the number of eggs and prints it to STDOUT
 puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # Prints a statement to STDOUT
-puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+puts 'Is it true that 3.0 + 2.0 < 5.0 - 7.0?'
 
 # Evaluates two equations and prints the result to STDOUT
 puts 3.0 + 2.0 < 5.0 - 7.0
@@ -26,7 +26,7 @@ puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
 puts "Oh, that's why it's false."
 
 # Prints a statement to STDOUT
-puts "How about some more."
+puts 'How about some more.'
 
 # Calculates an equation and prints the result to STDOUT
 puts "It is greater? #{5.0 > -2.0}"
