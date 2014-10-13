4ba97ac1b47dc3702a96e9814871057779307ac9
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-13 23:15:52 +1100

    Corrected issues identified by rubocop

--- ex04.rb
+++ ex04.rb
@@ -15,7 +15,6 @@ carpool_capacity = cars_driven * space_in_a_car
 # Calculate and set the average passengers per car
 average_passengers_per_car = passengers / cars_driven
 
-
 puts "There are #{cars} cars available."
 puts "There are only #{drivers} drivers available."
 puts "There will be #{cars_not_driven} empty cars today."
