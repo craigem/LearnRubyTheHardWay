401f4d172a508d82dd28b9da30c5f05331c9ebbe
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-16 08:49:38 +1100

    Corrected issues identified by rubocop to ex12

--- ex12.rb
+++ ex12.rb
@@ -1,10 +1,10 @@
-print "Give me a number: "
+print 'Give me a number: '
 number = gets.chomp.to_f
 
 bigger = number * 100
 puts "A bigger number is #{bigger}."
 
-print "Give me a another number: "
+print 'Give me a another number: '
 another = gets.chomp
 number = another.to_f
 
