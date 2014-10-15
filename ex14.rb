958ee4be882ed448aa716e157e0685c8ee974c7a
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-16 08:55:49 +1100

    Corrected issues identified by rubocop to ex14

--- ex14.rb
+++ ex14.rb
@@ -6,15 +6,17 @@ puts "I'd like to ask you a few questions."
 puts "Do you like me #{user_name}? ", prompt
 likes = $stdin.gets.chomp
 
-puts  "Where do you live #{user_name}? ", prompt
+puts "Where do you live #{user_name}? ", prompt
 lives = $stdin.gets.chomp
 
-puts "What kind of computer do you have? ", prompt
+puts 'What kind of computer do you have? ', prompt
 computer = $stdin.gets.chomp
 
-puts """
+msg = %(
 Alright, so you said #{likes} about liking me.
 You live in #{lives}. Not sure where that is.
 And you have a #{computer} computer. Nice.
 The day of the week is #{dow}. Cool
-"""
+)
+
+puts msg
