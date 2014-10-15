79e54e545bc7bcc0e451b64eb9be364b7d5730da
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-16 08:56:48 +1100

    Corrected issues identified by rubocop to ex15

--- ex15.rb
+++ ex15.rb
@@ -6,7 +6,7 @@ puts "Here's your file #{filename}:"
 print txt.read
 txt.close
 
-print "type the filename again: "
+print 'type the filename again: '
 file_again = $stdin.gets.chomp
 
 txt_again = open(file_again)
