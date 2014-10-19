0093b555d040059c4448e4eb631fca1ea749dfdb
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-19 20:21:42 +1100

    Corrected issues identified by rubocop to ex25

--- ex25.rb
+++ ex25.rb
@@ -1,46 +1,45 @@
+# Exercise 25, Learn Python the Hard Way
 module Ex25
-
   # This function will break up words for us.
-  def Ex25.break_words(stuff)
+  def self.break_words(stuff)
     words = stuff.split(' ')
-    return words
+    words
   end
 
   # Sorts the words.
-  def Ex25.sort_words(words)
-    return words.sort
+  def self.sort_words(words)
+    words.sort
   end
 
   # Prints the first word after popping it off.
-  def Ex25.print_first_word(words)
+  def self.print_first_word(words)
     word  = words.shift
     puts word
   end
 
   # Prints the last word after popping it off.
-  def Ex25.print_last_word(words)
+  def self.print_last_word(words)
     word  = words.pop
     puts word
   end
 
   # Takes in a full sentence and returns the sorted words.
-  def Ex25.sort_sentence(sentence)
+  def self.sort_sentence(sentence)
     words = Ex25.break_words(sentence)
-    return Ex25.sort_words(words)
+    Ex25.sort_words(words)
   end
 
   # Prints the first and last owrds of the sentence
-  def Ex25.print_first_and_last(sentence)
-    words = Ex25.break_words(sentence)
+  def self.print_first_and_last(sentence)
+    words.break_words(sentence)
     Ex25.print_first_word(words)
     Ex25.print_last_word(words)
   end
 
   # Sorts the words then prints the first and last one
-  def Ex25.print_first_and_last_sorted(sentence)
+  def self.print_first_and_last_sorted(sentence)
     words = Ex25.sort_sentence(sentence)
     Ex25.print_first_word(words)
     Ex25.print_last_word(words)
   end
-
 end
