e5cd1ddcbcaaa6d68efd4827eb8879fd21fcdf99
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-19 21:39:21 +1100

    Corrected issues identified by rubocop to ex34

--- ex34.rb
+++ ex34.rb
@@ -1,16 +1,16 @@
 animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
 
-# For each of these, write out a full sentence of the form: "The first (1st) 
-# animal is at 0 and is a bear." Then say it backwards: "The animal at 0 is the 
+# For each of these, write out a full sentence of the form: "The first (1st)
+# animal is at 0 and is a bear." Then say it backwards: "The animal at 0 is the
 # 1st animal and is a bear."
 #
 # Use your Ruby to check your answers.
 
-puts "The animal at 1 is " + animals[1]
-puts "The third (3rd) animal is " + animals[2]
-puts "The first (1st) animal is " + animals[0]
-puts "The animal at 3 is " + animals[3]
-puts "The fifth (5th) animal is " + animals[4]
-puts "The animal at 2 is " + animals[2]
-puts "The sixth (6th) animal is " + animals[5]
-puts "The animal at 4 is " + animals[4]
+puts 'The animal at 1 is ' + animals[1]
+puts 'The third (3rd) animal is ' + animals[2]
+puts 'The first (1st) animal is ' + animals[0]
+puts 'The animal at 3 is ' + animals[3]
+puts 'The fifth (5th) animal is ' + animals[4]
+puts 'The animal at 2 is ' + animals[2]
+puts 'The sixth (6th) animal is ' + animals[5]
+puts 'The animal at 4 is ' + animals[4]
