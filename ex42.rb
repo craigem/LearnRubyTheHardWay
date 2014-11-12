97ea44f16f374381301486fa46c2d27e32921f41
Craige McWhirter <craige@mcwhirter.com.au>
2014-11-13 10:16:29 +1100

    Finished typing up ex42

--- ex42.rb
+++ ex42.rb
@@ -2,7 +2,7 @@
 class Animal
 end
 
-## ??
+## Dog is-a Animal
 class Dog < Animal
 
   def initialize(name)
@@ -11,7 +11,7 @@ class Dog < Animal
   end
 end
 
-## ??
+## Cat is-a animal
 class Cat < Animal
 
   def initialize(name)
@@ -20,7 +20,7 @@ class Cat < Animal
   end
 end
 
-## ??
+## Person is-a object
 class Person
 
   def initialize(name)
@@ -34,7 +34,7 @@ class Person
   attr_accessor :pet
 end
 
-## ??
+## Employee is-a Person
 class Employee < Person
 
   def initialize(name, salary)
@@ -46,15 +46,15 @@ class Employee < Person
 
 end
 
-## ??
+## Fish is-a object
 class Fish
 end
 
-## ??
+## Salmon is-a Fish
 class Salmon < Fish
 end
 
-## ??
+## Halibut is-a Fish
 class Halibut < Fish
 end
 
@@ -62,26 +62,26 @@ end
 ## rover is-a Dog
 rover = Dog.new("Rover")
 
-## ??
+## satan is-a Cat
 satan = Cat.new("Satan")
 
-## ??
+## mary is-a Person
 mary = Person.new("Mary")
 
-## ??
+## Mary has-a Pet, satan
 mary.pet = satan
 
-## ??
+## Frank is-a Employee
 frank = Employee.new("Frank", 120000)
 
-## ??
+## Frank has-a pet, rover
 frank.pet = rover
 
-## ??
+## flipper is-a Fish
 flipper = Fish.new()
 
-## ??
+## crouse is-a Salmon
 crouse = Salmon.new()
 
-## ??
+## harry is-a Halibut
 harry = Halibut.new()
